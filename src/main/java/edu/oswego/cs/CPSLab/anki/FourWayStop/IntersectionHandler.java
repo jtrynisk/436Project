b34b24d3bed7c860a959b28f24d92367870be612
Benjamin Groman <benp.groman@gmail.com>
2019-05-02 12:55:18 -0400

    Fixed to actually do something useful.

--- src/main/java/edu/oswego/cs/CPSLab/anki/FourWayStop/IntersectionHandler.java
+++ src/main/java/edu/oswego/cs/CPSLab/anki/FourWayStop/IntersectionHandler.java
@@ -1,19 +1,27 @@
 package edu.oswego.cs.CPSLab.anki.FourWayStop;
 
-import de.adesso.anki.AdvertisementData;
-import de.adesso.anki.Vehicle;
-
 import java.util.Queue;
 
 /**
  * An interface to handle communication between cyber-physical vehicles.
  * @author Shakhar Dasgupta <sdasgupt@oswego.edu>
+ * @author Benjamin Groman <bgroman@oswego.edu>
  */
 public interface IntersectionHandler {
-    void awaitClearIntersection();
-    void broadcast(AdvertisementData vehicleInfo);
-    void clearIntersection();
-    void notify(Vehicle target, Queue queue);
-    void listenToBroadcast();
-    void becomeMaster();
+	//these three functions return false if an error was encountered
+    boolean broadcast(VehicleInfo vehicleInfo);
+    boolean clearIntersection();
+    boolean notify(VehicleInfo target);
+    
+    //returns true if and only if a clear was actually received,
+    //rather than a timeout
+    boolean awaitClearIntersection(int timeout);
+    //returns a queue of responders
+    //null indicates the timeout was reached
+    Queue<VehicleInfo> listenToBroadcast(int timeout);
+    //returns the queue received from the previous master,
+    //without the current vehicle
+    //empty queue indicates no one else is at the intersection
+    //null indicates the timeout was reached
+    Queue<VehicleInfo> becomeMaster(int timeout);
 }
