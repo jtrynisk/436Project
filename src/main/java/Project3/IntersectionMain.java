ae6fb15e4646dc382fd5893f55cbb73bce5cb936
Jondn Tryniski <jtrynisk@oswego.edu>
2019-05-08 19:30:21 -0400

    updated some stuff in intersectionMain

--- src/main/java/Project3/IntersectionMain.java
+++ src/main/java/Project3/IntersectionMain.java
@@ -5,6 +5,7 @@ import de.adesso.anki.RoadmapScanner;
 import de.adesso.anki.Vehicle;
 import de.adesso.anki.messages.*;
 import de.adesso.anki.roadmap.Roadmap;
+import edu.oswego.cs.CPSLab.anki.FourWayStop.CCNA;
 
 import java.util.Iterator;
 import java.util.List;
@@ -28,25 +29,19 @@ public class IntersectionMain {
             System.out.println("No cars found");
         }
 
+        Iterator<Vehicle> iter = vehicleList.iterator();
+
+
+        for(int i = 0; i < vehicleList.size(); i++){
+            Vehicle temp = iter.next();
+            if (temp.getAdvertisement().getIdentifier() == 'a'){}
+                //v = temp;
+
+        }
         Vehicle v = vehicleList.get(0);
         v.connect();
         v.sendMessage(new SdkModeMessage());
-        RoadmapScanner rs = new RoadmapScanner(v);
-        Roadmap rm = new Roadmap();
-        boolean isComplete = false;
-        /* This will build a roadmap but currently never exits the while loop.
-        while(!isComplete) {
-            rs.startScanning();
-            isComplete = rs.isComplete();
-        }
-        rm = rs.getRoadmap();
-        rs.stopScanning();
-        v.sendMessage(new SetSpeedMessage(0, 200));
-        try {
-            Thread.sleep(10000);
-        }catch(InterruptedException e){
-            e.printStackTrace();
-        }*/
+
         v.sendMessage(new SetSpeedMessage(200, 200));
         v.addMessageListener(LocalizationIntersectionUpdateMessage.class,
                 (message) -> transitionUpdateHandler(message, v));
@@ -55,10 +50,13 @@ public class IntersectionMain {
 
 
     public static void transitionUpdateHandler(LocalizationIntersectionUpdateMessage message, Vehicle v) {
+        CCNA cc = new CCNA();
         System.out.println(message.getIntersectionCode());
         if (message.getIntersectionCode() == 0){
             v.sendMessage(new SetSpeedMessage(0, 999999999));
             try {
+                //Needs vehicle info
+                //cc.broadcast();
                 Thread.sleep(3000);
             }catch (InterruptedException e){
                 e.printStackTrace();
